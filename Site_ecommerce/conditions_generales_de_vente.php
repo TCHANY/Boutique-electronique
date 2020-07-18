e77cb257f9e55121756537a7bfb87548c0d82100
TCHANY <48646336+TCHANY@users.noreply.github.com>
2020-07-18 16:48:24 +0200

    Add files via upload

---
+++ Site_ecommerce/conditions_generales_de_vente.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once('includes/header.php');
+require_once('includes/sidebar.php');
+
+?>
+
+<br/><h1>Conditions générales de vente</h1>
+
+<p>Les prix des marchandises vendues sont ceux en vigueur au jour de la prise de commande. Ils sont libellés en euros et calculés hors taxes. Par voie de conséquence, ils seront majorés du taux de TVA et des frais de transport applicables au jour de la commande.
+La société CaroleKadiatou s'accorde le droit de modifier ses tarifs à tout moment. Toutefois, elle s'engage à facturer les marchandises commandées aux prix indiqués lors de l'enregistrement de la commande.</p><br/>
+
+<?php
+require_once('includes/footer.php');
+
+?>
---
+++ Site_ecommerce/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+<h5>Copyright &copy; Boutique Electronique 2020</h5>
+</div>
